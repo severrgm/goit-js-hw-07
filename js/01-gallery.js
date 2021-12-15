f567e9e5fb390563a35472c3faf7a5bd7b37c247
Siversky Vladimir <rgm.ukraine@gmail.com>
2021-12-15 22:34:03 +0200

    221

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const divGalleryRef = document.querySelector(".gallery");
+
+function createGalleryImages(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                  <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                  />
+                </a>
+              </div>`;
+    })
+    .join("");
+}
+
+const createMarkup = createGalleryImages(galleryItems);
+
+divGalleryRef.insertAdjacentHTML("beforeend", createMarkup);
+
+let instance;
+
+function onImageZoomClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+
+  instance.show();
+  document.addEventListener("keydown", onModalCloseEscapePress);
+}
+
+function onModalCloseEscapePress(event) {
+  const pressKey = event.code;
+
+  if (pressKey !== "Escape") {
+    return;
+  }
+
+  instance.close();
+  document.removeEventListener("keydown", onModalCloseEscapePress);
+}
+
+divGalleryRef.addEventListener("click", onImageZoomClick);
