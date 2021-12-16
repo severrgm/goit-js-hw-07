adb8b77454e23618c6ffa57a0ee4029d2a385a45
Siversky Vladimir <rgm.ukraine@gmail.com>
2021-12-16 22:08:38 +0200

    7787

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const listGalleryRef = document.querySelector(".gallery");
+
+function createGalleryImages(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+const createMarkup = createGalleryImages(galleryItems);
+
+listGalleryRef.insertAdjacentHTML("beforeend", createMarkup);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
